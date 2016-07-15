07bf3c8841842082ff409a0dc7f87971e69c4273
KyoZeus <franciscopaivaknebel@gmail.com>
2016-07-15 12:43:40 -0300

    Added loading message to owned polls queue.

--- client/js/showPolls.controller.js
+++ client/js/showPolls.controller.js
@@ -3,14 +3,24 @@ var app = angular.module('poll', []);
 app.controller('showPolls', function($scope, $http) {
 	$scope.firstLoad = true;
 	$scope.message = "";
+	$scope.loading = false;
 
 	$scope.getPolls = function() {
 		if ($scope.firstLoad) {
+			$scope.loading = true;
 			$scope.firstLoad = false;
 
 			$http.get('/profile/getpolls').then(function(response) {
 				$scope.polls = response.data;
+				$scope.loading = false;
+				$scope.message = "";
+
+				if (response.data && response.data.length <= 0) {
+					$scope.message = "User has no owned polls."
+				}
+
 			}, function(response) {
+				$scope.loading = false;
 				console.log(response.status);
 				$scope.message = "Finding polls failed. Please try again later.";
 			});
