a0ae04cffcb011bfdb5b4d4cf6833264846324cb
kyozeus <franciscopaivaknebel@gmail.com>
2016-07-18 21:21:48 -0300

    Remove auth middleware that might have been breaking connect accounts.

--- server/routes/auth/facebook.js
+++ server/routes/auth/facebook.js
@@ -1,41 +1,41 @@
-module.exports = function(app, passport) {
-	app.get('/auth/facebook', isLoggedOut, passport.authenticate('facebook', {
+module.exports = function ( app, passport ) {
+	app.get( '/auth/facebook', isLoggedOut, passport.authenticate( 'facebook', {
 		scope: 'email'
-	}));
+	} ) );
 
-	app.get('/auth/facebook/callback', isLoggedOut, passport.authenticate('facebook', {
+	app.get( '/auth/facebook/callback', isLoggedOut, passport.authenticate( 'facebook', {
 		successRedirect: '/profile',
 		failureRedirect: '/'
-	}));
+	} ) );
 
-	app.get('/connect/facebook', isLoggedIn, passport.authorize('facebook', {
+	app.get( '/connect/facebook', passport.authorize( 'facebook', {
 		scope: 'email'
-	}));
+	} ) );
 
 	/*app.get('/connect/facebook/callback', isLoggedIn, passport.authorize('facebook', {
 		successRedirect: '/profile',
 		failureRedirect: '/'
 	}));*/
 
-	app.get('/unlink/facebook', isLoggedIn, function(req, res) {
+	app.get( '/unlink/facebook', isLoggedIn, function ( req, res ) {
 		var user = req.user;
 		user.facebook.token = undefined;
-		user.save(function(err) {
-			res.redirect('/profile');
-		});
-	});
+		user.save( function ( err ) {
+			res.redirect( '/profile' );
+		} );
+	} );
 }
 
-function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated())
+function isLoggedIn( req, res, next ) {
+	if ( req.isAuthenticated() )
 		return next();
 	else
-		res.redirect('/');
+		res.redirect( '/' );
 }
 
-function isLoggedOut(req, res, next) {
-	if (req.isUnauthenticated()) {
+function isLoggedOut( req, res, next ) {
+	if ( req.isUnauthenticated() ) {
 		return next();
 	}
-	res.redirect('/');
+	res.redirect( '/' );
 }
--- server/routes/auth/twitter.js
+++ server/routes/auth/twitter.js
@@ -1,39 +1,39 @@
-module.exports = function(app, passport) {
-	app.get('/auth/twitter', isLoggedOut, passport.authenticate('twitter'));
+module.exports = function ( app, passport ) {
+	app.get( '/auth/twitter', isLoggedOut, passport.authenticate( 'twitter' ) );
 
-	app.get('/auth/twitter/callback', isLoggedOut, passport.authenticate('twitter', {
+	app.get( '/auth/twitter/callback', isLoggedOut, passport.authenticate( 'twitter', {
 		successRedirect: '/profile',
 		failureRedirect: '/'
-	}));
+	} ) );
 
-	app.get('/connect/twitter', isLoggedIn, passport.authorize('twitter', {
+	app.get( '/connect/twitter', passport.authorize( 'twitter', {
 		scope: 'email'
-	}));
+	} ) );
 
 	/*app.get('/connect/twitter/callback', isLoggedIn, passport.authorize('twitter', {
 		successRedirect: '/profile',
 		failureRedirect: '/'
 	}));*/
 
-	app.get('/unlink/twitter', isLoggedIn, function(req, res) {
+	app.get( '/unlink/twitter', isLoggedIn, function ( req, res ) {
 		var user = req.user;
 		user.twitter.token = undefined;
-		user.save(function(err) {
-			res.redirect('/profile');
-		});
-	});
+		user.save( function ( err ) {
+			res.redirect( '/profile' );
+		} );
+	} );
 }
 
-function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated())
+function isLoggedIn( req, res, next ) {
+	if ( req.isAuthenticated() )
 		return next();
 	else
-		res.redirect('/');
+		res.redirect( '/' );
 }
 
-function isLoggedOut(req, res, next) {
-	if (req.isUnauthenticated()) {
+function isLoggedOut( req, res, next ) {
+	if ( req.isUnauthenticated() ) {
 		return next();
 	}
-	res.redirect('/');
+	res.redirect( '/' );
 }
